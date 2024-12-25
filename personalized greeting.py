925b9739e3977aee9b6d1c8d391d59214d591d1e
mitaliborad <mitaliia109@gmail.com>
2024-12-25 09:06:27 +0530

    Create personalized greeting.py

---
+++ personalized greeting.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+
+age = input("How old are you? ")
+color = input("What is your favourite color? ")
+
+print(f"Hello {name}! You are {age} Years old and your favourite color is {color}.")
